8048c5613f421a1946b5c890d4b324e473fa3075
MrActividades854 <kevinjoelescobar2020@gmail.com>
2026-05-04 19:49:24 -0500

    i have everything

--- frontend/pages/home.js
+++
@@ -1,9 +0,0 @@
-import { ProductCard } from "../components/productCard.js";
-
-export function ProductList(products) {
-  return `
-    <section class="product-list">
-      ${products.map(p => ProductCard(p)).join("")}
-    </section>
-  `;
-}
